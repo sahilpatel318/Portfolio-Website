9d2c603bfe141a53ca6788b380697bb14e9fdda7
Sahil Hareshbhai Patel <sahilpatel31082004@gmail.com>
2025-12-11 15:22:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/port/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
